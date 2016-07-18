8a8c3c18b006d9e4dbc310f59c8e214d6c3cc9d4
Jinseoung Lee <js824.lee@samsung.com>
2016-07-18 11:25:25 +0900

    Added Comment for radix sort

--- src/org/prudy/algorithms/sort/RadixSort.java
+++ src/org/prudy/algorithms/sort/RadixSort.java
@@ -6,6 +6,10 @@ package org.prudy.algorithms.sort;
  * @date 2016.07.14
  * 
  * Radix Sort
+ * 
+ * [ 설명 ]
+ * Radix(기수)는 숫자의 자릿수를 의미
+ * 각 Radix 별로 정렬을 진행하여, Radix sort라고하며 아래에는 각 기수별로 계수정렬(counting sort)함.
  */
 public class RadixSort implements Sort{
 	
@@ -20,18 +24,22 @@ public class RadixSort implements Sort{
 	 * @return sorted array
 	 */
 	public int[] sort(int[] array) {		
-		final int MAX_LENGTH = this.getMaxLength(array), ARRAY_LENGTH = array.length;
-		int powed = 1;
+		final int MAX_LENGTH = this.getMaxLength(array),	// 가장 큰 값의 자릿 수의 개수를 구해줌, 예를 들어 999 -> 3, 80 -> 2
+				ARRAY_LENGTH = array.length;				// Array의 Length 값
+		int powed = 1;	// 해당 자릿수를 가져오기 위해 위치 값 계산, (N/10^(k-1))%10 = N의 k번째 숫자, 이 수식에서 10^(k-1) 부분의 누적에 해당함
 		int[] sortedArray = new int[ARRAY_LENGTH], counts;
 		
 		for(int p = 0; p < MAX_LENGTH; p++){
+			// 아래는 계수정렬 방식으로 진행
 			counts = new int[10];
 			
 			for(int number : array){
 				counts[(number/powed) % 10]++;
 			}
 			
-			for(int i = 1; i < 10; i++ ) counts[i] += counts[i-1];
+			for(int i = 1; i < 10; i++ ){
+				counts[i] += counts[i-1];
+			}
 			
 			for(int i = ARRAY_LENGTH-1; i >= 0; i--){
 				sortedArray[counts[(array[i]/powed) %10]-- -1] = array[i];
